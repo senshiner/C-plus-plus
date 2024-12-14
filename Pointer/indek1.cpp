e37cd5a63028827d8087a9a216d94ac72e0a2626
Sendi Winoto <157441609+senshiner@users.noreply.github.com>
2024-12-14 21:23:50 +0700

    update Pointer/indek1.cpp

--- Pointer/indek1.cpp
+++ Pointer/indek1.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 // fungsi (&) mendapatkan alamat variabel
-// fungsi (*p) membuka variabel dan melihat isi didalamnya
+// fungsi (* ) membuka variabel dan melihat isi didalamnya
 
 int main(void) {
     int A = 25, *pA;             // Variabel A dengan nilai 25 dan pointer pA
