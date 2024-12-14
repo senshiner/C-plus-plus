265c9a84ab8e18d49df10570266a5ad6065aa047
Sendi Winoto <157441609+senshiner@users.noreply.github.com>
2024-12-14 20:30:59 +0700

    update Pointer/alamat_array.cpp

--- Pointer/alamat_array.cpp
+++ Pointer/alamat_array.cpp
@@ -1,9 +1,19 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
+
 // fungsi (&) mendapatkan alamat variabel
-int main(void){
-    int a[7];
-    cout << "Alamat a[0] ="<<&a[0]<<endl;
-    cout << "Alamat a[1] ="<<&a[1]<<endl;
-    cout << "Alamat a[2] ="<<&a[2]<<endl;
+int main(void) {
+    int a[7];  // Array a dengan 7 elemen, tetapi belum diinisialisasi dengan nilai
+
+    // Menampilkan alamat a[0]
+    cout << "Alamat a[0] = " << &a[0] << endl;
+
+    // Menampilkan alamat a[1]
+    cout << "Alamat a[1] = " << &a[1] << endl;
+
+    // Menampilkan alamat a[2]
+    cout << "Alamat a[2] = " << &a[2] << endl;
+
+    // Karena array a tidak diinisialisasi, nilai dari a[0], a[1], dll. tidak diketahui
+    // Namun, alamat dari elemen tetap valid meskipun nilainya belum diatur
 }
