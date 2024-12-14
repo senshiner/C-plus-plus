4fe7420036336a07111f637b0a2ee87aebcf0bec
Sendi Winoto <157441609+senshiner@users.noreply.github.com>
2024-12-14 20:35:35 +0700

    update Pointer/hitung_char.cpp

--- Pointer/hitung_char.cpp
+++ Pointer/hitung_char.cpp
@@ -1,26 +1,31 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
+
 // fungsi (&) mendapatkan alamat variabel
 // fungsi (* ) membuka variabel dan melihat isi didalamnya
+
 int main(void)
 {
-    char kalimat[] = {"Selamat Belajar Visual C++"};
-    char *pKarakter;
-    int JumlahHurufKecil = 0;
-    int JumlahHurufBesar = 0;
+    char kalimat[] = {"Selamat Belajar Visual C++"};  // Inisialisasi array karakter
+    char *pKarakter;                                // Pointer untuk karakter
+    int JumlahHurufKecil = 0;                       // Variabel untuk menghitung huruf kecil
+    int JumlahHurufBesar = 0;                       // Variabel untuk menghitung huruf besar
 
-    pKarakter = kalimat;
+    pKarakter = kalimat;                            // Pointer menunjuk ke awal array
 
-    while (*pKarakter)
+    while (*pKarakter)                              // Looping sampai pointer mencapai akhir string
     {
-        char kar = *pKarakter;// && dibaca "dan"
-        if (kar >= 'a' && kar <= 'z')
+        char kar = *pKarakter;                      // Mengambil karakter yang ditunjuk oleh pointer
+        
+        // && berarti "dan", artinya kondisi akan menjadi true hanya jika kedua kondisi terpenuhi
+        if (kar >= 'a' && kar <= 'z')               // Mengecek apakah karakter adalah huruf kecil
             JumlahHurufKecil++;
-        if (kar >= 'A' && kar <= 'Z')
+        if (kar >= 'A' && kar <= 'Z')               // Mengecek apakah karakter adalah huruf besar
             JumlahHurufBesar++;
-        pKarakter++;
+        
+        pKarakter++;                                // Melangkah ke karakter berikutnya
     }
 
     cout << "Jumlah Huruf Kecil = " << JumlahHurufKecil << endl;
     cout << "Jumlah Huruf Kapital = " << JumlahHurufBesar << endl;
-} 
+}
