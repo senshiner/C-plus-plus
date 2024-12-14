58ff5530e9f6beb6c294e15613d3fca27cf34fd9
Sendi Winoto <157441609+senshiner@users.noreply.github.com>
2024-12-14 20:49:12 +0700

    update Pointer/indek1.cpp

--- Pointer/indek1.cpp
+++ Pointer/indek1.cpp
@@ -1,17 +1,33 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
+
 // fungsi (&) mendapatkan alamat variabel
 // fungsi (* ) membuka variabel dan melihat isi didalamnya
-int main(void){
-    int A=25,*pA;
-    pA=&A;
-    cout<<"A="<<A<<endl;
-    cout<<"pA="<<pA<<endl;
-    cout<<"&A="<<&A<<endl;
-    cout<<"&pA="<<&pA<<endl;
-    cout<<"*pA="<<*pA<<endl;
-    cout<<endl;
-    *pA=100;
-    cout<<"*pA="<<*pA<<endl;
+
+int main(void) {
+    int A = 25, *pA;             // Variabel A dengan nilai 25 dan pointer pA
+    pA = &A;                      // Pointer pA menunjuk ke alamat A
+
+    // output: 25
+    cout << "A = " << A << endl;
+
+    // output: alamat &A
+    cout << "pA = " << pA << endl;
+
+    // output: alamat &A
+    cout << "&A = " << &A << endl;
+
+    // output: alamat &pA
+    cout << "&pA = " << &pA << endl;
+
+    // output: 25
+    cout << "*pA = " << *pA << endl;
+
+    cout << endl;
+
+    *pA = 100; // Mengubah nilai A melalui pointer pA
+
+    // output: 100
+    cout << "*pA = " << *pA << endl;
 
 }
