0901ae78cc95fcac29653ebc90282e0a6d154283
Sendi Winoto <157441609+senshiner@users.noreply.github.com>
2024-12-14 20:21:20 +0700

    update Pointer/abc.cpp

--- Pointer/abc.cpp
+++ Pointer/abc.cpp
@@ -1,14 +1,19 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 // fungsi (&) mendapatkan alamat variabel
 // fungsi (* ) membuka variabel dan melihat isi didalamnya
-int main(void){
-    char A[3][5]={'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O'};
+int main(void) {
+    // Inisialisasi array 2D 'A' dengan nilai karakter
+    char A[3][5] = {'A', 'B', 'C', 'D', 'E',
+                    'F', 'G', 'H', 'I', 'J',
+                    'K', 'L', 'M', 'N', 'O'};
 
+    // Pointer P dengan karakter(char)
     char *P;
-    P=&A[0][0];
 
-    cout<<*P<<endl;
+    // array 2d ini berati baris index 0 dan kolon index 0 yaitu A jadi sama aja mencari &A
+    P = &A[0][0];
 
-
+    // Outputnya akan muncul isi alamat &A
+    cout << *P << endl;
 }
