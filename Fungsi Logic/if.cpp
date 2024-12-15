e5be88f2c98e248ed91e75419f172dc12fcd510d
Sendi Winoto <157441609+senshiner@users.noreply.github.com>
2024-12-15 23:43:27 +0700

    update Fungsi Logic/if.cpp

--- Fungsi Logic/if.cpp
+++ Fungsi Logic/if.cpp
@@ -1,14 +1,17 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-main(void)
-{
-	int x,s;
-	cout << "Masukan Bilangan Bulat: ";
-	cin >> x;
-	s = x % 2;
 
-	if(s == 0)
-	cout << "Bilangan Genap" << endl;
-    else
-    cout << "Bilangan Ganjil" << endl;
+int main(void) {
+    int x, s; // Deklarasi variabel x,s integer
+    cout << "Masukan Bilangan Bulat: ";
+    cin >> x; // Input angka x
+    s = x % 2; // Variabel s = sisa pembagian x dengan 2 (modulus)
+
+    // Struktur if-else
+    if (s == 0) // Kondisi: jika s (sisa pembagian x dengan 2) sama dengan 0
+        cout << "Bilangan Genap" << endl; // Jika kondisi terpenuhi, menampilkan "Bilangan Genap"
+    else // Jika kondisi tidak terpenuhi
+        cout << "Bilangan Ganjil" << endl; // Menampilkan "Bilangan Ganjil"
+
+    return 0;
 }
