27fa88bbaaeb70aebb491231a145ee48c19e572c
Sendi Winoto <157441609+senshiner@users.noreply.github.com>
2024-12-16 20:52:06 +0700

    update Fungsi-Logic/array_karakter.cpp

--- Fungsi-Logic/array_karakter.cpp
+++ Fungsi-Logic/array_karakter.cpp
@@ -15,17 +15,18 @@ int main(void) {
     cout << nama[0] << endl; // Output: J
     cout << nama[7] << endl; // Output: e
 
-    char kalimatA[35];
-    char kalimatB[35];
-    strcpy(kalimatA, "Kota Yogyakarta"); // Output: Kota Yogyakarta
-    strcpy(kalimatB, kalimatA); // strcpy: salin kalimatA ke kalimatB
-    strupr(kalimatA); // strupr: konversi ke huruf besar
-    cout << kalimatA << endl; // Output: KOTA YOGYAKARTA
-
-    strlwr(kalimatA); // strlwr: ubah ke huruf kecil
-    cout << kalimatA << endl; // Output: kota yogyakarta
-
-    cout << strcmp(kalimatA, kalimatB) << endl; // strcmp: membandingkan kalimatA dan kalimatB
-    strcat(kalimatB, " Never Ending Asia"); // strcat: menambahkan string ke akhir kalimatB
-    cout << kalimatB << endl;              
+  /*=====
+  Kode dibawah hanya bisa di Runing di editor support strupr dan strlwr
+  =====*/
+    // char kalimatA[35];
+    // char kalimatB[35];
+    // strcpy(kalimatA, "Kota Yogyakarta"); // Output: Kota Yogyakarta
+    // strcpy(kalimatB, kalimatA); // strcpy: salin kalimatA ke kalimatB
+    // strupr(kalimatA); // strupr: konversi ke huruf besar
+    // cout << kalimatA << endl; // Output: KOTA YOGYAKARTA
+    // strlwr(kalimatA); // strlwr: ubah ke huruf kecil
+    // cout << kalimatA << endl; // Output: kota yogyakarta
+    // cout << strcmp(kalimatA, kalimatB) << endl; // strcmp: membandingkan kalimatA dan kalimatB
+    // strcat(kalimatB, " Never Ending Asia"); // strcat: menambahkan string ke akhir kalimatB
+    // cout << kalimatB << endl;
 }
